7189d6da59f1f3c22c69bf500e5e12284c002655
Afdhan <77567725+Afdhan@users.noreply.github.com>
2023-08-15 09:37:12 +0700

    #

---
+++ menu/m-ssws.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+MYIP=$(wget -qO- ipv4.icanhazip.com);
+acc
+clear
+echo -e "\033[0;35m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
+echo -e "\E[45;1;30m             SHADOWSOCKS MENU             \E[0m"
+echo -e "\033[0;35m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
+echo -e ""
+echo -e " [\e[36m•1\e[0m] Create Account Shadowsocks "
+echo -e " [\e[36m•2\e[0m] Create Trial Shadowsocks "
+echo -e " [\e[36m•3\e[0m] Extending Account Shadowsocks "
+echo -e " [\e[36m•4\e[0m] Delete Account Shadowsocks "
+echo -e " [\e[36m•5\e[0m] User list created Account "
+echo -e ""
+echo -e " [\e[31m•0\e[0m] \e[31mBACK TO MENU\033[0m"
+echo -e   ""
+echo -e   "Press x or [ Ctrl+C ] • To-Exit"
+echo ""
+echo -e "\033[0;35m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
+echo -e ""
+read -p " Select menu : " opt
+echo -e ""
+case $opt in
+1) clear ; add-ssws ;;
+2) clear ; trialssws ;;
+3) clear ; renew-ssws ;;
+4) clear ; del-ssws ;;
+5) clear ; cat /etc/log-create-shadowsocks.log ; exit ;;
+0) clear ; menu ;;
+x) exit ;;
+*) echo "Wrong Press!n" ; sleep 1 ; m-ssws ;;
+esac
---
+++ menu/m-system.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+MYIP=$(wget -qO- ipv4.icanhazip.com);
+acc
+clear
+echo -e "\033[0;35m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
+echo -e "\E[45;1;30m                SYSTEM MENU               \E[0m"
+echo -e "\033[0;35m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
+echo -e ""
+echo -e " [\e[36m•1\e[0m] Domain Panel"
+echo -e " [\e[36m•2\e[0m] Speedtest VPS"
+echo -e " [\e[36m•3\e[0m] Set Auto Reboot"
+echo -e " [\e[36m•4\e[0m] Restart All Service"
+echo -e " [\e[36m•5\e[0m] Cek Bandwith"
+echo -e " [\e[36m•6\e[0m] Install TCP BBR"
+echo -e " [\e[36m•7\e[0m] DNS CHANGER"
+echo -e ""
+echo -e " [\e[31m•0\e[0m] \e[31mBACK TO MENU\033[0m"
+echo -e   ""
+echo -e   "Press x or [ Ctrl+C ] • To-Exit"
+echo -e   ""
+echo -e "\033[0;35m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
+echo -e ""
+read -p " Select menu : " opt
+echo -e ""
+case $opt in
+1) clear ; m-domain ; exit ;;
+2) clear ; speedtest ; exit ;;
+3) clear ; auto-reboot ; exit ;;
+4) clear ; restart ; exit ;;
+5) clear ; bw ; exit ;;
+6) clear ; m-tcp ; exit ;;
+7) clear ; m-dns ; exit ;;
+0) clear ; menu ; exit ;;
+x) exit ;;
+*) echo -e "" ; echo "Wrong Press!" ; sleep 1 ; m-system ;;
+esac
