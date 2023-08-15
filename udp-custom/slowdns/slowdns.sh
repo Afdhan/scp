e748e3405355d13d00fe5cffc165d9cde59d8a16
Afdhan <77567725+Afdhan@users.noreply.github.com>
2023-08-15 11:28:37 +0700

    #

--- udp-custom/slowdns/slowdns.sh
+++ udp-custom/slowdns/slowdns.sh
@@ -46,6 +46,9 @@ service cron restart
 cd
 # echo "Port 2222" >> /etc/ssh/sshd_config
 # echo "Port 2269" >> /etc/ssh/sshd_config
+
+sed -i '/Port 22/a Port 2222' /etc/ssh/sshd_config
+sed -i '/Port 22/a Port 2269' /etc/ssh/sshd_config
 sed -i 's/#AllowTcpForwarding yes/AllowTcpForwarding yes/g' /etc/ssh/sshd_config
 service ssh restart
 service sshd restart
